3b20ea4ab63baf664d5f291e963bfb8aa5cbb32a
goncalveshigino <goncalveshigino20@gmail.com>
2021-06-08 12:55:43 +0100

    detalhes

--- lib/src/pages/carros/carro_page.dart
+++ lib/src/pages/carros/carro_page.dart
@@ -13,6 +13,35 @@ class CarroPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(carro.nome),
         centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.place),
+            onPressed: _onClickMapa,
+          ),
+          IconButton(
+            icon: Icon(Icons.videocam),
+            onPressed: _onClickVideo,
+          ),
+          PopupMenuButton<String>(
+            onSelected: _onClickPopupMenu,
+            itemBuilder: (BuildContext context) {
+              return [
+                PopupMenuItem(
+                  value: "Editar",
+                  child: Text("Editar"),
+                ),
+                PopupMenuItem(
+                  value: "Deletar",
+                  child: Text("Deletar"),
+                ),
+                PopupMenuItem(
+                  value: "Share",
+                  child: Text("Share"),
+                ),
+              ];
+            },
+          )
+        ],
       ),
       body: _body(),
     );
@@ -20,8 +49,84 @@ class CarroPage extends StatelessWidget {
 
   _body() {
     return Container(
-      padding: const EdgeInsets.all(18),
-      child: Image.network(carro.urlFoto)
+      margin: EdgeInsets.all(2),
+      padding: EdgeInsets.only(left: 9),
+      child: ListView(
+        scrollDirection: Axis.vertical,
+        children: [
+          Image.network(carro.urlFoto), 
+          _bloco1(), 
+          SizedBox(height: 20,),
+          _bloco2()
+        ],
+      ),
+    );
+  }
+
+  Row _bloco1() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              carro.nome,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              overflow: TextOverflow.ellipsis,
+            ),
+            Text(
+              carro.tipo,
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            IconButton(
+              icon: Icon(Icons.favorite, color: Colors.red, size: 30),
+              onPressed: _onClickFavorite,
+            ),
+            IconButton(
+              icon: Icon(Icons.share, size: 30),
+              onPressed: _onClickShared,
+            )
+          ],
+        )
+      ],
+    );
+  }
+
+  void _onClickMapa() {}
+
+  void _onClickVideo() {}
+
+  _onClickPopupMenu(String value) {
+    switch (value) {
+      case "Editar":
+        print("Editar !!!");
+        break;
+      case "Deletar":
+        print("Deletar !!!");
+        break;
+      case "Share":
+        print("Share !!!");
+        break;
+    }
+  }
+
+  void _onClickFavorite() {}
+
+  void _onClickShared() {}
+
+  _bloco2() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text( carro.descricao, style: TextStyle( fontSize: 16, fontWeight: FontWeight.w500)), 
+        SizedBox(height: 12,),
+        Text( 'hdhdhdddddddddddddddddddhfjhfjhfhjfhjfhjfhjfjhfhjfhjfjhfhjfjhfhjfjhfjhfjheuytetydeyteyteyte675e75e75etuydshgvxdghxcghcgcgjcjgcjgcghcgcgjcgjcgjcgjcgjcgjgjgjcjgcgjdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddhgfhgfrhfreuihreuiyrteuiyrteuiyerthyuiferhifigfgfsghkfhgdfkhgdfkhgfgdhgjkdfgjkhdfuhgfruyertyireiyteruyerouyifrhjuvdfjbldvklvcxbljvfsgoudflhgdfggljsfglgshlkgshkghlkghljgshjgskhgsgkhfgiysfgufruoyryuiterugefrjgvsfbjvsxljhbsvlkhvslihdgugorfegouefrihorgelihfgdlkbbdlbkhvbljgdfggougfhidfsg;khdfglhiglhgusgguvljbdgflhidgfougreugorfgihrgelihdrgugbfbfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfgfggfgfgfgfgfgfgfgfgfgfgfgfghydydydyfyfyfyfhfvhugfoihfgohgdfhlbgfhljbljncv ljnbvljnbgfnjbvmjkdjkodjodjodnojfbjiofbgopjigeopjighoputghpuigdfhpuidfsgihpugdfihymkjxvhljxvblkhx vbkhcx bkh cxkhbx cbkhx cbjkcx khb cxbjkcxbjkx ckhbcvxkhbcvbkhvckhbvxckhbvxckhbxcvhblkvxchbkcvxbkhxcvbkhxcvbkhcvxkhbxcvkhbxcvkhbxcvkhbxcvbjkhxcvbkhxcvbkhxcvkhbxcvbjkhcvxbjkhcvxkhbvxckhbcvxbkhcvxkhbcvxkhbjvxckhbvxckbjhcvxbjkhvxcbjkhcvxbjhjfhkfvdjhbcvjhbcvjhxc jhvx cjhvxcvvhcxkhxchvjxcjhvxcjhvxcvjhcxhvjxcjhvkjfufdudfiydfiy', style: TextStyle( fontSize: 16, fontWeight: FontWeight.w300))
+      ],
     );
   }
 }
